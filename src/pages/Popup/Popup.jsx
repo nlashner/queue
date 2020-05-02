ae752b862f1db2af8837b1bc1933833d77c4ecb3
Nora Lashner <nlashner@gmail.com>
2020-05-02 08:46:52 -0400

    can pass the url to the back

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,25 +3,59 @@ import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
-const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Popup extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      url: '',
+      title: '',
+      status: ''
+    }
+    this.saveClick = this.saveClick.bind(this)
+  }
+
+  async saveClick() {
+    let math = Math.floor(Math.random() * Math.floor(188))
+    let app = this
+
+    // await chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //   let activeWindow = tabs[0]
+    //   let url = activeWindow.url
+    //   let title = activeWindow.title
+
+    //   app.setState({
+    //     status: 'saved!',
+    //     url: url,
+    //     title: title,
+    //   })
+    // })
+
+    const request = new XMLHttpRequest()
+    await request.open('GET', 'https://cat-fact.herokuapp.com/facts', true)
+    request.onload = function () {
+      const data = JSON.parse(this.response)
+      const fact = data.all[math].text
+      app.setState({
+        status: fact
+      })
+    }
+    request.send()
+    console.log(this.state)
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.saveClick}>Save this Track</button>
+          <p>{this.state.status}</p>
+        </header>
+      </div>
+    );
+  }
 };
 
 export default Popup;
+
